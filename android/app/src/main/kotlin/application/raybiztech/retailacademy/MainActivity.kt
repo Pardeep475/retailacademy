c4c5bdd3ae51d19b8c7736572e5821a915cea81a
Pardeep <pardeepsharma475@gmail.com>
2022-11-23 11:15:44 +0530

    registration issue resolved in zoom meeting

--- android/app/src/main/kotlin/application/raybiztech/retailacademy/MainActivity.kt
+++ android/app/src/main/kotlin/application/raybiztech/retailacademy/MainActivity.kt
@@ -9,6 +9,10 @@ import us.zoom.sdk.*
 class MainActivity : FlutterActivity() {
     private val ZOOM_INTEGRATION_CHANNEL = "application.raybiztech.retailacademy/ZOOM_INTEGRATION"
 
+    var userName: String = ""
+    var userEmail: String = ""
+
+
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         initializeSdk()
@@ -18,7 +22,8 @@ class MainActivity : FlutterActivity() {
             ZOOM_INTEGRATION_CHANNEL
         ).setMethodCallHandler { call, result ->
             if (call.method == "JOIN_MEETING") {
-                val userName = call.argument<String>("USER_NAME")
+                userName = call.argument<String>("USER_NAME").toString()
+                userEmail = call.argument<String>("USER_EMAIL").toString()
                 val meetingId = call.argument<String>("MEETING_ID")
                 val meetingPassword = call.argument<String>("MEETING_PASSWORD")
 
@@ -26,7 +31,7 @@ class MainActivity : FlutterActivity() {
                 println("METHOD_CHANNEL_PARAMS:- MEETING_ID $meetingId")
                 println("METHOD_CHANNEL_PARAMS:- MEETING_PASSWORD $meetingPassword")
 
-                if (userName != null && meetingId != null && meetingPassword != null) {
+                if (meetingId != null && meetingPassword != null) {
                     joinMeeting(
                         userName = userName,
                         meetingId = meetingId,
@@ -46,9 +51,9 @@ class MainActivity : FlutterActivity() {
         // TODO: Do not use hard-coded values for your key/secret in your app in production!
         val params = ZoomSDKInitParams().apply {
             appKey =
-                "HIMamIALS7Xj2at8j5RcveUGDWvtlmhXyRzR" // TODO: Retrieve your SDK key and enter it here
+                "SIT5cR2lSchMAg1lvdx6bUfvx9JflLGDSGRM" // TODO: Retrieve your SDK key and enter it here
             appSecret =
-                "VYQ0baZxhNE7GtxfbgDM3KLX1jemNtpVL1kq" // TODO: Retrieve your SDK secret and enter it here
+                "y7qEJ22BO9Zhs2XlPN6OP3Oty5xJGMHUmzWJ" // TODO: Retrieve your SDK secret and enter it here
             domain = "zoom.us"
             enableLog = true // Optional: enable logging for debugging
         }
@@ -58,21 +63,251 @@ class MainActivity : FlutterActivity() {
              * If the [errorCode] is [ZoomError.ZOOM_ERROR_SUCCESS], the SDK was initialized and can
              * now be used to join/start a meeting.
              */
-            override fun onZoomSDKInitializeResult(errorCode: Int, internalErrorCode: Int) = Unit
+            override fun onZoomSDKInitializeResult(errorCode: Int, internalErrorCode: Int) : Unit{
+
+            }
             override fun onZoomAuthIdentityExpired() = Unit
         }
 
         sdk.initialize(this@MainActivity, listener, params)
+
     }
 
-    private fun joinMeeting(userName: String, meetingId: String, meetingPassword: String) {
+    private fun joinMeeting(
+        userName: String,
+        meetingId: String,
+        meetingPassword: String,
+    ) {
         val meetingService = ZoomSDK.getInstance().meetingService
         val options = JoinMeetingOptions()
+        options.no_webinar_register_dialog = true
         val params = JoinMeetingParams().apply {
             displayName = userName
             meetingNo = meetingId
             password = meetingPassword
         }
         meetingService.joinMeetingWithParams(this@MainActivity, params, options)
+
+        ZoomSDK.getInstance()?.inMeetingService?.addListener(meetingServiceListener)
     }
+
+
+    private val meetingServiceListener = object : InMeetingServiceListener{
+        override fun onMeetingNeedPasswordOrDisplayName(
+            p0: Boolean,
+            p1: Boolean,
+            p2: InMeetingEventHandler?
+        ) {
+            p2?.setRegisterWebinarInfo(userName,userEmail,false)
+        }
+
+        override fun onWebinarNeedRegister(p0: String?) {
+            println("METHOD_CHANNEL_PARAMS:- $p0")
+        }
+
+        override fun onJoinWebinarNeedUserNameAndEmail(p0: InMeetingEventHandler?) {
+            p0?.setRegisterWebinarInfo(userName,userEmail,false)
+
+        }
+
+        override fun onMeetingNeedCloseOtherMeeting(p0: InMeetingEventHandler?) {
+            
+        }
+
+        override fun onMeetingFail(p0: Int, p1: Int) {
+            
+        }
+
+        override fun onMeetingLeaveComplete(p0: Long) {
+            
+        }
+
+        override fun onMeetingUserJoin(p0: MutableList<Long>?) {
+            
+        }
+
+        override fun onMeetingUserLeave(p0: MutableList<Long>?) {
+            
+        }
+
+        override fun onMeetingUserUpdated(p0: Long) {
+            
+        }
+
+        override fun onMeetingHostChanged(p0: Long) {
+            
+        }
+
+        override fun onMeetingCoHostChanged(p0: Long) {
+            
+        }
+
+        override fun onMeetingCoHostChange(p0: Long, p1: Boolean) {
+            
+        }
+
+        override fun onActiveVideoUserChanged(p0: Long) {
+            
+        }
+
+        override fun onActiveSpeakerVideoUserChanged(p0: Long) {
+            
+        }
+
+        override fun onHostVideoOrderUpdated(p0: MutableList<Long>?) {
+            
+        }
+
+        override fun onFollowHostVideoOrderChanged(p0: Boolean) {
+            
+        }
+
+        override fun onSpotlightVideoChanged(p0: Boolean) {
+            
+        }
+
+        override fun onSpotlightVideoChanged(p0: MutableList<Long>?) {
+            
+        }
+
+        override fun onUserVideoStatusChanged(
+            p0: Long,
+            p1: InMeetingServiceListener.VideoStatus?
+        ) {
+            
+        }
+
+        override fun onUserNetworkQualityChanged(p0: Long) {
+            
+        }
+
+        override fun onSinkMeetingVideoQualityChanged(p0: VideoQuality?, p1: Long) {
+            
+        }
+
+        override fun onMicrophoneStatusError(p0: InMeetingAudioController.MobileRTCMicrophoneError?) {
+            
+        }
+
+        override fun onUserAudioStatusChanged(
+            p0: Long,
+            p1: InMeetingServiceListener.AudioStatus?
+        ) {
+            
+        }
+
+        override fun onHostAskUnMute(p0: Long) {
+            
+        }
+
+        override fun onHostAskStartVideo(p0: Long) {
+            
+        }
+
+        override fun onUserAudioTypeChanged(p0: Long) {
+            
+        }
+
+        override fun onMyAudioSourceTypeChanged(p0: Int) {
+            
+        }
+
+        override fun onLowOrRaiseHandStatusChanged(p0: Long, p1: Boolean) {
+            
+        }
+
+        override fun onChatMessageReceived(p0: InMeetingChatMessage?) {
+            
+        }
+
+        override fun onChatMsgDeleteNotification(p0: String?, p1: ChatMessageDeleteType?) {
+            
+        }
+
+        override fun onShareMeetingChatStatusChanged(p0: Boolean) {
+            
+        }
+
+        override fun onSilentModeChanged(p0: Boolean) {
+            
+        }
+
+        override fun onFreeMeetingReminder(p0: Boolean, p1: Boolean, p2: Boolean) {
+            
+        }
+
+        override fun onMeetingActiveVideo(p0: Long) {
+            
+        }
+
+        override fun onSinkAttendeeChatPriviledgeChanged(p0: Int) {
+            
+        }
+
+        override fun onSinkAllowAttendeeChatNotification(p0: Int) {
+            
+        }
+
+        override fun onSinkPanelistChatPrivilegeChanged(p0: InMeetingChatController.MobileRTCWebinarPanelistChatPrivilege?) {
+            
+        }
+
+        override fun onUserNameChanged(p0: Long, p1: String?) {
+            
+        }
+
+        override fun onUserNamesChanged(p0: MutableList<Long>?) {
+            
+        }
+
+        override fun onFreeMeetingNeedToUpgrade(p0: FreeMeetingNeedUpgradeType?, p1: String?) {
+            
+        }
+
+        override fun onFreeMeetingUpgradeToGiftFreeTrialStart() {
+            
+        }
+
+        override fun onFreeMeetingUpgradeToGiftFreeTrialStop() {
+            
+        }
+
+        override fun onFreeMeetingUpgradeToProMeeting() {
+            
+        }
+
+        override fun onClosedCaptionReceived(p0: String?, p1: Long) {
+            
+        }
+
+        override fun onRecordingStatus(p0: InMeetingServiceListener.RecordingStatus?) {
+            
+        }
+
+        override fun onLocalRecordingStatus(
+            p0: Long,
+            p1: InMeetingServiceListener.RecordingStatus?
+        ) {
+            
+        }
+
+        override fun onInvalidReclaimHostkey() {
+            
+        }
+
+        override fun onPermissionRequested(p0: Array<out String>?) {
+            
+        }
+
+        override fun onAllHandsLowered() {
+            
+        }
+
+        override fun onLocalVideoOrderUpdated(p0: MutableList<Long>?) {
+            
+        }
+
+    }
+
+
 }
